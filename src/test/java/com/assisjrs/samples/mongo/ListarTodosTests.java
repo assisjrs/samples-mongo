540a56253ea68a968505d696faef59f668ee2a54
GGTeodoro <ggteodor07@gmail.com>
2021-04-16 12:17:46 -0300

    Primeiro test TDD

---
+++ src/test/java/com/assisjrs/samples/mongo/ListarTodosTests.java
@@ -0,0 +1,23 @@
+package com.assisjrs.samples.mongo;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.DEFINED_PORT;
+
+@AutoConfigureWebTestClient
+@SpringBootTest(webEnvironment = DEFINED_PORT)
+class ListarTodosTests {
+	@Autowired
+	private WebTestClient webTestClient;
+
+	@Test
+	void retornaStatus200() {
+		webTestClient.get().uri("/livros").exchange().expectStatus().isOk();
+	}
+
+}
