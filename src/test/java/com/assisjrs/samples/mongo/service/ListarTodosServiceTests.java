476b2276ca2252b91153913bd3d9184459d58d39
GGTeodoro <ggteodor07@gmail.com>
2021-04-16 17:11:00 -0300

    Teste Service de Integração

--- src/test/java/com/assisjrs/samples/mongo/service/ListarTodosServiceTests.java
+++ src/test/java/com/assisjrs/samples/mongo/service/ListarTodosServiceTests.java
@@ -1,59 +1,42 @@
 package com.assisjrs.samples.mongo.service;
 
-import java.util.List;
-import java.util.ArrayList;
-
 import com.assisjrs.samples.mongo.Entity.Livro;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-
-import org.mockito.Mock;
-import org.mockito.InjectMocks;
-import static org.mockito.Mockito.*;
-import org.mockito.junit.jupiter.MockitoExtension;
-
 import com.assisjrs.samples.mongo.repository.LivroRepository;
-
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import static org.assertj.core.api.Assertions.assertThat;
 
-@ExtendWith(MockitoExtension.class)
+
+import java.util.List;
+
+@SpringBootTest
 public class ListarTodosServiceTests {
-    @InjectMocks
+    @Autowired
     private LivroService livroService;
 
-    @Mock
+    @Autowired
     private LivroRepository livroRepository;
 
-    @Test
-    void naoDeveRetonarNuloAoListarLivros() {
-        List<?> retornoListarTodos = livroService.listarTodos();
-        assertThat(retornoListarTodos).isNotNull();
-    }
-
-    @Test
-    void deveUsarRepositorioParaListarLivros() {
-        livroService.listarTodos();
-        verify(livroRepository, atLeastOnce()).findAll();
-    }
-
-    @Test
-    void deveManterOrdemDeEntradaDeLivros() {
-        List<Livro> retornoListarTodos = new ArrayList<>();
-
-        Livro livro1 = new Livro("1", "Senhor Dos aneis : A Sociedade do Anel");
-        retornoListarTodos.add(livro1);
+    @BeforeEach
+    void antesDeCadaTeste() {
+        Livro livro = new Livro("1", "Senhor Dos aneis : A Sociedade do Anel");
+        livroRepository.save(livro);
 
         Livro livro2 = new Livro("2", "Senhor Dos aneis : As Duas Torres");
-        retornoListarTodos.add(livro2);
-
-        when(livroRepository.findAll()).thenReturn(retornoListarTodos);
-
-        List<Livro> listaComparacao = new ArrayList<>();
-        listaComparacao.add(livro1);
-        listaComparacao.add(livro2);
-
-        List<Livro> retornoServiceListarTodos = livroService.listarTodos();
-        assertThat(retornoServiceListarTodos).asList().containsAll(listaComparacao);
+        livroRepository.save(livro2);
     }
 
+    @Test
+    void deveRetornarListaDeLivrosDoBanco() {
+        List<Livro> livros = livroService.listarTodos();
+        assertThat(livros).isNotEmpty();
+    }
+
+    @Test
+    void deveConterSegundoLivroDaLista() {
+        List<Livro> livros = livroService.listarTodos();
+        assertThat(livros.get(1)).isEqualTo(new Livro("2", "Senhor Dos aneis : As Duas Torres"));
+    }
 }
